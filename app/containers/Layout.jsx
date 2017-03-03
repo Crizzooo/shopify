6462acd5dd265bdda5b474679a146aafe74cbf64
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-03-03 15:11:51 -0500

    feat(completed background image, overlay, and controlButtons on product cards

--- app/containers/Layout.jsx
+++ app/containers/Layout.jsx
@@ -17,53 +17,56 @@ class Layout extends Component {
   render () {
     return (
       <div>
-        <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+          <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
 
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <a className="navbar-brand" id="logo" href="#">Final Vinyl </a>
 
-          <a className="navbar-brand" id="logo" href="#">Final Vinyl </a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Products
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a className="dropdown-item" href="#">Albums</a>
+                          <a className="dropdown-item" href="#">Artists</a>
+                          <a className="dropdown-item" href="#">Merchandise</a>
+                        </div>
+                </li>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item dropdown">
-                      <a className="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Products
-                      </a>
-                      <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a className="dropdown-item" href="#">Albums</a>
-                        <a className="dropdown-item" href="#">Artists</a>
-                        <a className="dropdown-item" href="#">Merchandise</a>
-                      </div>
-              </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Checkout</a>
+                </li>
 
-              <li className="nav-item">
-                <a className="nav-link" href="#">Checkout</a>
-              </li>
+              </ul>
+              <form className="form-inline my-2 my-lg-0"onSubmit={evt => {
+                evt.preventDefault()
+                login(evt.target.username.value, evt.target.password.value)
+              } }>
+                <input className="form-control mr-sm-1" placeholder="Username" name="username" />
+                <input className="form-control mr-sm-1" placeholder="Password" name="password" type="password" />
+                <input className="btn btn-outline-success my-2 my-sm-0" type="submit" value="Login" />
+              </form>
+            </div>
 
-            </ul>
-            <form className="form-inline my-2 my-lg-0"onSubmit={evt => {
-              evt.preventDefault()
-              login(evt.target.username.value, evt.target.password.value)
-            } }>
-              <input className="form-control mr-sm-1" placeholder="Username" name="username" />
-              <input className="form-control mr-sm-1" placeholder="Password" name="password" type="password" />
-              <input className="btn btn-outline-success my-2 my-sm-0" type="submit" value="Login" />
-            </form>
+          </nav>
+          <div className="bodyBackground">
+            <div className="bodyOverlay">
+
+              <div className="container content">
+                { this.props.children }
+              </div>
+
+              <hr />
+
+              <div id="footer" className="container">
+                FINYL VINYL | 5 Hanover Square 11th Fl. New York, NY. 10022 | 212.333.4444
+              </div>
+            </div>
           </div>
-
-        </nav>
-
-        <div className="container content">
-          { this.props.children }
-        </div>
-
-        <hr />
-
-        <div id="footer" className="container">
-          FINYL VINYL | 5 Hanover Square 11th Fl. New York, NY. 10022 | 212.333.4444
-        </div>
       </div>
     );
   }
