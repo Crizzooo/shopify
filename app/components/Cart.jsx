9e72c2feb61a2ecbba7f42ca8dacb5480e26f08a
John Underwood <john.w.underwood.iii@gmail.com>
2017-03-04 18:12:19 -0500

    feat(cart): cart calculates qty of each object in cart

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -14,14 +14,34 @@ export default class Cart extends Component {
   render() {
 
     const cartItem = this.props.cartItem
-    console.log('in dumb component, cartItem is:', cartItem)
+    const checkStock = this.props.checkStock
+    const quantity = 2 //hard-coded for now
 
     return (
-      <div>
-            {
-              <ul key={cartItem} className="text-muted"> product id: {cartItem.product_id} </ul>
-            }
-      </div>
-    )
+        <tr>
+          <td className="col-sm-8 col-md-6">
+          <div className="media">
+              <a className="thumbnail pull-left" href="#"> <img className="media-object" src="http://www.designformusic.com/wp-content/uploads/2015/10/insurgency-digital-album-cover-design.jpg" width="50" height="50"/> </a>
+              <div>
+                  <h4 className="media-heading"><a href="#" className="text-success">{cartItem.product.title}</a></h4>
+                  <span>Status: </span>{checkStock(cartItem.product.quantity)}
+              </div>
+          </div></td>
+          <td className="col-sm-1 col-md-1" >
+          <input type="email" className="form-control" id="exampleInputEmail1" value="1" />
+          </td>
+          <td className="col-sm-1 col-md-1 text-center"><strong>{`$${cartItem.product.price}`}</strong></td>
+          <td className="col-sm-1 col-md-1 text-center"><strong>${(cartItem.product.price* quantity).toFixed(2)}</strong></td>
+          <td className="col-sm-1 col-md-1">
+          <button type="button" className="btn btn-danger">
+              <span className="glyphicon glyphicon-remove"></span> Remove
+          </button></td>
+        </tr>
+            )
   }
 }
+    //   <div>
+    //         {
+    //           <ul key={cartItem} className="text-muted"> product id: {cartItem.product_id} </ul>
+    //         }
+    //   </div>
--- app/containers/CartContainer.jsx
+++ app/containers/CartContainer.jsx
@@ -8,21 +8,93 @@ class CartContainer extends React.Component {
 
   constructor(props) {
     super(props)
+    this.checkStock = this.checkStock.bind(this)
+    this.subtotal = 0
   }
 
 
+  checkStock (qty) {
+    return qty > 0 ?
+    <span className="text-success"><strong>In Stock </strong></span> :
+    <span className="text-danger"><strong> Out of Stock </strong></span>
+  }
+
   render () {
     const cart = this.props.cart;
+    const priceArray = cart && cart.map(cartItem => +cartItem.product.price)
+    console.log('cart is:', cart)
+    let itemQty = {}
+    itemQty = cart && Object.assign(itemQty, cart.forEach(cartItem => {
+          if(itemQty[cartItem.product_id]) itemQty[cartItem.product_id] ++
+            else itemQty[cartItem.product_id] = 1
+        // Ternary set off linter
+        // itemQty[cartItem.product_id] ?
+        // itemQty[cartItem.product_id]++ :
+        // itemQty[cartItem.product_id] = 1
+    }))
+
+
     return (
-            <div>
-              <h2 className="text-success">Your Cart</h2>
-              <div className="flexContainer">
-              {cart && cart.map(cartItem => (
-                <Cart cartItem={cartItem} key={cartItem.id}/>
-                ))
-            }
-              </div>
-            </div>
+  <div className="container">
+    <div className="row">
+        <div className="col-sm-12 col-md-10 col-md-offset-1">
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th className="text-center">Price</th>
+                        <th className="text-center">Total</th>
+                        <th> </th>
+                    </tr>
+                </thead>
+                <tbody> {
+                  cart && cart.map(cartItem => (
+                    <Cart cartItem={cartItem} checkStock={this.checkStock} key={cartItem.id} />
+                ))}
+                    <tr>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                        <td><h5>Subtotal</h5></td>
+                        <td className="text-right"><h5><strong>${
+                          priceArray && priceArray.reduce((total, price) => {
+                            return total + price
+                          }).toFixed(2)
+                        }</strong></h5></td>
+                    </tr>
+                    <tr>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                        <td><h5>Estimated shipping</h5></td>
+                        <td className="text-right"><h5><strong>$6.94</strong></h5></td>
+                    </tr>
+                    <tr>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                        <td><h3>Total</h3></td>
+                        <td className="text-right"><h3><strong>$31.53</strong></h3></td>
+                    </tr>
+                    <tr>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>   </td>
+                        <td>
+                        <button type="button" className="btn btn-default">
+                            <span className="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
+                        </button></td>
+                        <td>
+                        <button type="button" className="btn btn-success">
+                            Checkout <span className="glyphicon glyphicon-play"></span>
+                        </button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
             )
   }
 }
@@ -43,3 +115,14 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartContainer);
+
+
+// <div>
+//               <h2 className="text-success">Your Cart</h2>
+//               <div className="flexContainer">
+//               {cart && cart.map(cartItem => (
+//                 <Cart cartItem={cartItem} key={cartItem.id}/>
+//                 ))
+//             }
+//               </div>
+//             </div>
