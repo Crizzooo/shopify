afdf63f4c299464cc24a7aa4e969cc5d24955029
John Underwood <john.w.underwood.iii@gmail.com>
2017-03-06 19:16:03 -0500

    feat(cart): add, edit, remove all work

--- db/models/cartItem.js
+++ db/models/cartItem.js
@@ -4,6 +4,23 @@
 const Sequelize = require('sequelize')
 const db = require('APP/db')
 
-const CartItem = db.define('cartItems', {})
+const CartItem = db.define('cartItems', {
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1
+  },
+  price_paid: {
+    type: Sequelize.DECIMAL(10, 2)
+  }
+},
+  {
+    hooks: {
+      afterSave(cartItem) {
+        if(cartItem.quantity === 0) {
+          return cartItem.destroy()
+      }
+    }
+  }
+})
 
 module.exports = CartItem
